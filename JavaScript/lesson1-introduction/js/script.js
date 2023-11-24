247934c84758999f2d1cccaa181831ecc29d0b76
HasanBakhtiar <hasan.doctype@gmail.com>
2023-11-24 21:06:06 +0400

    js enter

---
+++ JavaScript/lesson1-introduction/js/script.js
@@ -0,0 +1,16 @@
+// console.log("Hello man");
+// one line
+/**
+ * 
+ * more 
+ * line
+ */
+// console.log("Hello1");
+// console.log("Hello2");
+// debugger;
+// consoale.log("Hello3");
+// console.log("Hello4");
+// console.log("Hello5");
+
+
+// 
---
+++ JavaScript/lesson2-data-types/js/script.js
@@ -0,0 +1,21 @@
+// DATA TYPES
+// Primitive DT
+// string
+// number
+// boolean
+
+// Composite DT
+// Array
+// var info = [5,"Hello",true,[1,2,3,4,5]];
+// console.log(info);
+// Object
+// var info = {
+//     name:"Shaiq"
+// }
+// console.log(info.name);
+
+// Special DT
+// var a = null;
+// console.log(a);
+// null
+// undefined
