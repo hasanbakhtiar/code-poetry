d721966f5538cb8e6bd8240b6b1b7fa55556885c
HasanBakhtiar <hasan.doctype@gmail.com>
2023-11-28 13:59:26 +0400

    lastest

---
+++ JavaScript/lesson3-operators/js/script.js
@@ -0,0 +1,113 @@
+
+
+
+// assignment operators
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+// arithmetic operators 
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+
+
+
+
+// comparison operators
+// ==
+// ===
+// !=
+// !==
+// < 
+// <= 
+// > 
+// >= 
+
+
+// var x = "f";
+// var y = "A";
+
+// if (x>y) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// logical operators
+// && - AND
+// || - OR
+// !  - NOT
+
+// if (false && true || !(5<10) ) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// string operators
+// +
+// +=
+
+// var a = "Hello";
+// a += "Mello"; 
+// console.log(a);
+
+
+
+// var infoOne = "Javascript";
+// infoOne += " Hello";
+
+// console.log(infoOne);
+
+
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+var a = 5 , b = 10;
+console.log(a+b);
+
+
+// ,            (vergül)(kodları bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+// var a = 5,  b= 10
+// console.log(a,b);
+
+var info = 'Shaiq\'s books';
+console.log(typeof 5);
+
+
+// \            (tərs slash)(Qaçındırma operatorudur)
+// var info = 'JavaScript\'s Book';
+// console.log(info); 
+
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+
+
+
+
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+// var a = 5;
+
+
+var a = 5;
+
+console.log(void a);
+
+
+
+
+
+    
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------
