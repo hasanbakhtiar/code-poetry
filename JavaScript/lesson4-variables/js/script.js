22cd67bd62527bb0ae609312092f50b149a7c660
HasanBakhtiar <hasan.doctype@gmail.com>
2023-12-05 01:55:10 +0400

    lastest

---
+++ JavaScript/lesson4-variables/js/script.js
@@ -0,0 +1,67 @@
+// var abstract = 5;
+// var abstract = 15;
+// console.log(abstract);
+
+// 1.Dəyişkən adları mənalı və anlaşıqlı olmalıdır.
+// 2.Dəyişkən adlarını yazarkən İngilis əlifbasından başqa əlifadan istifadə etmək qadağandır.
+// 3.Dəyişkən adları _ (alt xətt) ilə başlaya bilər.
+// 4.Dəyişkən adları $(dollar) işarəsi ilə başlaya bilər.
+// 5.Dəyişkən adları heç bir zaman rəqəm ilə başlaya bilməz.Amma ilk hərfdə sonra istifadə edə bilərik.
+// 6.Dəyişkən adları böyük kiçik hərfləri anlayır.
+// 7.Eyni dəyişkənə iki fərqli məlumat tanımlanan zaman sonuncu tanımlanan məlumatı göstərir.
+// 8.JavaScript tərəfindən hazırlanmış rezerv kəlimələri dəyişkən adlarına vermək qadağandır.
+
+// abstract 	    arguments	    await*	            boolean
+// break	        byte	        case	            catch
+// char	            class*	        const	            continue
+// debugger	        default	        delete	            do
+// double	        else	        enum*	            eval
+// export*	        extends*	    false	            final
+// finally	        float	        for	                function
+// goto	            if	            implements	        import*
+// in	            instanceof	    int	                interface
+// let*	            long	        native	            new
+// null	            package	        private	            protected
+// public	        return	        short	            static
+// super*	        switch	        synchronized	    this
+// throw	        throws	        transient	        true
+// try	            typeof	        var	                void
+// volatile	        while	        with	            yield
+
+// var      -> variable (global)
+// const    -> constant (local)
+// let      -> let      (local)
+
+
+
+// const a  = 5;
+//  a = 10;
+// console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+let a = 10;
+
+let info = 10;
+switch (a) {
+    case 10:
+        
+        console.log(info);
+        
+        break;
+
+    default:
+        break;
+}
+
+
+
---
+++ JavaScript/lesson6-loops/js/script.js
@@ -0,0 +1,21 @@
+
+// while(a>10){
+//     a++;
+//     console.log(a);
+// }
+
+
+// let a = 1;
+
+// do {
+//     console.log(a);
+//     a++;
+// }while(a<10)
+
+
+// for(let i=0;i<10;i++){
+//     if (i==5) {
+//         break;
+//     }
+//     console.log(i);
+// }
