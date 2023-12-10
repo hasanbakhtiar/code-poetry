8789e6cb653abe609cd060cd43292285b95b5bce
HasanBakhtiar <hasan.doctype@gmail.com>
2023-12-10 12:42:05 +0400

    array

---
+++ JavaScript/lesson7-array-object/js/script.js
@@ -0,0 +1,77 @@
+// const brand = new Array("Samsung","Acer","Asus") 
+
+// const brand = [];
+// brand[0] = "Acer";
+// brand[1] = "Hp";
+// brand[2] = "Lenovo";
+// brand[0] = "Lenovo"
+// brand[4] = "test"
+
+// brand['hello'] = "Hello";
+// console.log(brand['hello']);
+// const brand = ["Samsung","Acer",[1,2,3,[true,['test1','test2',['aim']],false],4,5],"Asus"];
+
+// console.log(brand[2][3][1][2][0]);
+
+
+// console.log(brand.length+brand[2].length);
+// brand.shift()
+
+// console.log(brand.slice(1,3));
+// const brand = ["Samsung","Acer","Asus",'Hp','Xiaomi'];
+
+// for (let a in brand) {
+//     console.log(a);
+//         console.log(brand[a]);
+
+// }
+
+
+// for(let a of brand){
+//     console.log(a);
+// }
+
+// for (let i = 0; i < brand.length; i++) {
+//    console.log(brand[i]);
+
+// }
+
+
+
+
+
+// const data =
+//     [
+//         {
+//             title: "BMW x3",
+//             price: 30000,
+//             newCar: false
+//         },
+//         {
+//             title: "Audi a3",
+//             price: 12000,
+//             newCar: true
+//         },
+//     ]
+
+// for(let i in data){
+//     console.log(data[i].title);
+// }
+
+
+
+// brand.splice(1,4,"Hello","Mello");
+// console.log(brand);
+
+// let productOne = ['bread','water','oil']; 
+// let productTwo = ['onion','milk','egg']; 
+// productOne.push(...productTwo)
+// let productFour = productOne;
+// console.log(productFour);
+// console.log(...[...productOne]);
+
+
+
+// for(let a of productFour){
+//     console.log(a);
+// }
