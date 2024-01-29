67d204de76193df75613d984d9c50e75c6467870
HasanBakhtiar <hasan.doctype@gmail.com>
2024-01-29 20:57:13 +0400

    lastest

---
+++ ReactJS/lesson2-components/src/App.jsx
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+import List from './List';
+
+class App extends Component {
+    render() {
+        return (
+            <>
+                <h1>This app for Monster</h1>
+                <List />
+            </>
+        )
+    }
+}
+
+export default App;
---
+++ ReactJS/lesson2-components/src/List.jsx
@@ -0,0 +1,15 @@
+import  { Component } from 'react'
+
+ class List extends Component {
+  render() {
+    return (
+      <ul>
+        <li>React JS</li>
+        <li>Redux JS</li>
+        <li>Next JS</li>
+      </ul>
+    )
+  }
+}
+
+export default List
